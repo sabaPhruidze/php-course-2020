7453694ce2c6e8c0b870132e71cae9b486e38239
Saba <sabaphruidze01@gmail.com>
2026-06-03 12:05:38 +0400

    feat(string):moving on learning strings

--- 04_strings.php
+++ 04_strings.php
@@ -1,9 +1,15 @@
 <?php
 
 // Create simple string
+$name = 'Saba';
+$string = 'Hello I m $name';
+$string2 = "Hello I'm $name";
+
+echo $string.'<br>';
+echo $string2.'<br>';
 
 // String concatenation
-
+echo 'Hello ' . 'World' . 'and PHP'. '<br/>';
 // String functions
 
 // Multiline text and line breaks
