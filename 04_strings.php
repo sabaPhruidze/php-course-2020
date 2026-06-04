d33ecca46d42c601f95e2e1d1e3f0ba523038a11
Saba <sabaphruidze01@gmail.com>
2026-06-04 14:19:27 +0400

    feat(04_strings):learned string methods ,how to save long text and how to do concatenation

--- 04_strings.php
+++ 04_strings.php
@@ -2,33 +2,48 @@
 
 // Create simple string
 $name = 'Saba';
-$string = 'Hello I m $name';
+$string = 'Hello I am $name';
 $string2 = "Hello I'm $name";
 
-echo $string.'<br>';
-echo $string2.'<br>';
+echo 'ერთი წერტილით '.$string.'<br>';
+echo 'ორი წერტილით '.$string2.'<hr/>';
 
 // String concatenation
-echo 'Hello ' . 'World' . 'and PHP'. '<br/>';
+echo 'Hello ' . 'World ' . 'and PHP'. '<hr/>';
 // String functions
 $basic = "   Hello World   ";
+var_dump($basic);
+echo '<br/>';
+echo "1 - strlen() ".strlen($basic) . '<br/>'; //length
+echo "2 - trim() ";
+var_dump(trim($basic));
+echo '<br/>';
 
-echo "1 - ".strlen($basic) . '<br/>'; //length
-echo "2 - ".trim($basic) . '<br/>';
-echo "3 - ".ltrim($basic) . '<br/>';
-echo "4 - ".rtrim($basic) . '<br/>';
-echo "5 - ".str_word_count($basic) . '<br/>';
+echo "3 - ltrim() ";
+var_dump(ltrim($basic));
+echo '<br/>';
+
+echo "4 - rtrim() ";
+var_dump(rtrim($basic));
+echo '<br/>';
+
+echo "5 - ". str_word_count($basic) . '<br/>';
 echo "6 - ".strrev($basic) . '<br/>';
 echo "7 - ".strtoupper($basic) . '<br/>';
 echo "8 - ".strtolower($basic) . '<br/>';
 echo "9 - ".ucfirst('hello') . '<br/>';
 echo "10 - ".lcfirst('HELLO') . '<br/>';
 echo "11 - ".ucwords("$basic and php") . '<br/>';
-echo '12 - '.strpos($basic,'world').'<br/>';
-echo '13 - '.stripos($basic,'world').'<br/>';
-echo '14 - '.substr($basic,8,6).'<br/>';
-echo '15 - '.str_replace('world','PHP',$basic).'<br/>';
-echo '16 - '.str_ireplace('world','PHP',$basic).'<br/>';
+echo '12 - '.strpos($basic,'world').'<br/>'; //ეძებს ტექსტს დიდი და პატარა ასოების გათვალისწინებით (case-sensitive)
+/*
+აქ $basic შეიცავს: Hello World მაგრამ შენ ეძებ: world
+World ≠ world ამიტომ შედეგი იქნება: false
+*/
+echo '13 - '.stripos($basic,'world').'<br/>';// ეძებს ტექსტს დიდი და პატარა ასოების გაუთვალისწინებლად (case-insensitive).
+
+echo '14 - '.substr($basic,8,6).'<br/>';// substr(სტრინგი, საიდან დაიწყოს, რამდენი სიმბოლო)
+echo '15 - '.str_replace('world','PHP',$basic).'<br/>'; //(არ ჩაანაცვლა) ტექსტის შეცვლა (ასოების ზომის გათვალისწინებით)
+echo '16 - '.str_ireplace('world','PHP',$basic).'<br/>'; //(ჩაანაცვლა) ტექსტის შეცვლა (ასოების ზომის გაუთვალისწინებლად)
 
 // Multiline text and line breaks
 // Multiline text and reserve html tags
@@ -39,6 +54,6 @@ I do sometimes coding
 ";
 echo '<hr/>'.$longText;
 echo '<br/>'.nl2br($longText).'<br/>'; // ინარჩუნებს ფორმას ახალი ხაზების მიხედვით
-echo '<br/>'.nl2br(htmlentities($longText));
-
+echo '<br/>'.nl2br(htmlentities($longText));// html კოდსაც აღიქვავს და დამატებით ახალ ხაზზე გადაჰყავს
+//New Line To <br>
 // https://www.php.net/manual/en/ref.strings.php
